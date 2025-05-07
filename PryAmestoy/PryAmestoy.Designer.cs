b1883e246caf5354f7c5a978d0e58a1f7c96110f
EstebanAmestoy <105952353+EstebanAmestoy@users.noreply.github.com>
2025-05-07 12:54:08 -0300

    Add files via upload

---
+++ PryAmestoy/PryAmestoy.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PryAmestoy
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmbSuperheroes = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CmbSuperheroes
+            // 
+            this.CmbSuperheroes.FormattingEnabled = true;
+            this.CmbSuperheroes.Location = new System.Drawing.Point(313, 26);
+            this.CmbSuperheroes.Name = "CmbSuperheroes";
+            this.CmbSuperheroes.Size = new System.Drawing.Size(121, 21);
+            this.CmbSuperheroes.TabIndex = 0;
+            this.CmbSuperheroes.SelectedIndexChanged += new System.EventHandler(this.CmbSuperheroes_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(219, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 255);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CmbSuperheroes);
+            this.Name = "FrmPrincipal";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CmbSuperheroes;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ PryAmestoy/PryAmestoy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryAmestoy
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void CmbSuperheroes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CmbSuperheroes.Items.Add("Batman");
+            CmbSuperheroes.Items.Add("Superman");
+            CmbSuperheroes.Items.Add("Ironman");
+            CmbSuperheroes.Items.Add("Spiderman");
+            CmbSuperheroes.Items.Add("hulk");
+
+            if ()
+            {
+
+            }
+        }
+
+    }
+}
